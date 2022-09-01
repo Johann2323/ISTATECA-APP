108fb3aed9baec1cc71f9854da1f215f6bd34d30
Johann Arizaga <johann.arizaga.est@tecazuay.edu.ec>
2022-09-01 01:20:44 -0500

    dl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import{HttpClientModule} from '@angular/common/http';
 
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { FormComponent } from './registro-usuario/form.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,18 +28,21 @@ import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
     ClientesComponent,
     ProveedorComponent,
     RegistroUsuarioComponent,
-    InicioSesionComponent
+    InicioSesionComponent,
+    FormComponent
 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot([
       {path: 'app-directiva', component: DirectivaComponent},
       {path: 'app-clientes', component: ClientesComponent},
       {path: 'app-proveedor', component: ProveedorComponent},
       {path: 'app-registro-usuario', component: RegistroUsuarioComponent},
       {path: 'app-inicio-sesion', component: InicioSesionComponent},
+      {path: 'app-form', component: FormComponent},
 
     ]),
   ],
