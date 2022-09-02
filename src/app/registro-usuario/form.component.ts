6dec493bf5eabca7f505d3b4e258300539932f4a
Johann Arizaga <johann.arizaga.est@tecazuay.edu.ec>
2022-09-02 01:42:17 -0500

    jds

--- src/app/registro-usuario/form.component.ts
+++ src/app/registro-usuario/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,6 +10,12 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  SaveData(form:NgForm){
+    console.log("enviando datos...")
+    console.log(form);
+    console.log(form.value);
+
   }
 
 }
