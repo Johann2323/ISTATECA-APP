e71525ae00ff7544d1e52c6fc1e7f4f3dcc15f08
LizethOrellana <lizeth.orellana.est@tecazuay.edu.ec>
2022-09-13 00:31:09 -0500

    listar bibliotecarios

--- src/app/lista-bibliotecarios/lista-bibliotecarios.component.ts
+++ src/app/lista-bibliotecarios/lista-bibliotecarios.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RegistroBibliotecarioService } from '../registro-bibliotecario/registro-bibliotecario.service';
+import { bibliotecario } from '../registro-bibliotecario/bibliotecario';
+import { persona } from '../persona';
 @Component({
   selector: 'app-lista-bibliotecarios',
   templateUrl: './lista-bibliotecarios.component.html',
   styleUrls: ['./lista-bibliotecarios.component.css']
 })
 export class ListaBibliotecariosComponent implements OnInit {
+  bibliotecarios:bibliotecario[]=[];
+  val:String="";
 
-  constructor() { }
+  constructor(private bibliotecarioservice: RegistroBibliotecarioService) { }
 
   ngOnInit(): void {
+    this.bibliotecarioservice.obtenerBibliotecarios().subscribe(
+      bibliotecarios=>this.bibliotecarios=bibliotecarios
+    );
   }
 
 }
--- src/app/registro-bibliotecario/registro-bibliotecario.service.ts
+++ src/app/registro-bibliotecario/registro-bibliotecario.service.ts
@@ -7,11 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class RegistroBibliotecarioService {
   private urlendpoint:string='http://localhost:8080/api/crearbibliotecario';
+  private urlendpoint1:string='http://localhost:8080/api/listarbibliotecario';
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
   constructor(private http:HttpClient) { }
 
   create(bibliotecario:bibliotecario):Observable<bibliotecario>{
     return this.http.post<bibliotecario>(this.urlendpoint, bibliotecario, {headers: this.httpHeaders})
+  }
+  obtenerBibliotecarios(): Observable<bibliotecario[]> {
+    //return of(CLIENTES)
+    return this.http.get<bibliotecario[]>(this.urlendpoint1);
   }
 }
