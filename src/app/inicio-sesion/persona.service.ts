7daebaf3b87dd8c957ea6bfec56a0984d336e20a
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2022-09-17 18:49:46 -0500

    rol

--- src/app/inicio-sesion/persona.service.ts
+++ src/app/inicio-sesion/persona.service.ts
@@ -8,13 +8,16 @@ import { map, Observable } from 'rxjs';
 })
 export class PersonaService {
   private urlendpoint: string = 'http://localhost:8080/api/validarLogin';
+  private urlendpoint1: string = 'http://localhost:8080/api/num_rol';
   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' })
   constructor(private http: HttpClient) { }
   validarLogin(usuario: String, clave: String):Observable<boolean>{
     let res=this.urlendpoint+"?usuario="+usuario+"&clave="+clave;
     return this.http.get<boolean>(res);
-
   }
-   
+  num_rol(usuario: String):Observable<number>{
+    let res=this.urlendpoint1+"?usuario="+usuario;
+    return this.http.get<number>(res);
+  }
   
 }
