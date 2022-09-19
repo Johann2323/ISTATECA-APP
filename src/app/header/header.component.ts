2fcc2d91fef010a5e574588b70568a0556b4f1b8
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2022-09-18 19:48:13 -0500

    roles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,30 +1,56 @@
-import { InvokeFunctionExpr } from "@angular/compiler";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, DoCheck, Input } from "@angular/core";
 import { PaginaInicioComponent } from "../pagina-inicio/pagina-inicio.component";
 import { InicioSesionComponent } from "../inicio-sesion/inicio-sesion.component";
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
-    templateUrl:'./header.component.html'
+    templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit{
-    reporteV:String="";
-    mostrar:boolean=false;
-    constructor(private router1:Router){}
-    ngOnInit(): void {
-        this.reporteV = JSON.parse(localStorage.getItem("idProducto")+"");
-        if(parseInt(this.reporteV+"")==9){
-            this.mostrar=false;
-            console.log(InicioSesionComponent.rol+"Holaaaaaaaaa2")
-        }else if(parseInt(this.reporteV+"")==2){
-            this.mostrar=true;
-            console.log("Llego");
+export class HeaderComponent implements DoCheck {
+    reporteV: String = "";
+    mostrar: boolean = false;
+    mostrarr: boolean = false;
+    mostrar1: boolean = false;
+    mostrar2: boolean = false;
+    mostrar3: boolean = false;
+    constructor(private router1: Router) { }
+    ngDoCheck(): void {
+        InicioSesionComponent.rol;
+        console.log("Cambio en rol a:" + InicioSesionComponent.rol)
+        if (InicioSesionComponent.rol == 0) {
+            this.mostrar = true;
+            this.mostrarr = true;
+            this.mostrar2 = true;
+        } else if (InicioSesionComponent.rol == 9) {
+            this.mostrar = false
+        } else if (InicioSesionComponent.rol == 2) {
+            this.mostrar = true;
+            this.mostrar2 = false;
+            this.mostrarr = false;
+        } else if (InicioSesionComponent.rol == 1) {
+            this.mostrar = true;
+            this.mostrar2 = true;
+            this.mostrarr = false;
         }
     }
-    cerrarSesion(){
-        this.router1.navigate(['']);
-        InicioSesionComponent.rol=9;
+
+    ngOnInit(): void {
+        console.log("Rol inicia en header con:" + InicioSesionComponent.rol)
+        if (InicioSesionComponent.rol == 9) {
+            this.mostrar = false
+        }
     }
+
+    cerrarSesion() {
+        this.router1.navigate(['']);
+        InicioSesionComponent.rol = 9;
+        this.ngOnInit;
+        console.log("Cerrar Sesion")
+    }
+
+
 }
+
+
 
--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -11,30 +11,21 @@ import { HeaderComponent } from '../header/header.component';
 
 })
 export class InicioSesionComponent implements OnInit {
-  public usuariopru: number = 7;
   respuesta: boolean = false;
   Persona: persona = new persona;
-  public static rol: number = 9;
-  constructor(private personaservice: PersonaService, private router: Router, private router1: Router) { }
+  public static rol: number=9;
+  constructor(private personaservice: PersonaService, private router: Router) { }
   ngOnInit(): void {
   }
   notificacion(email: String, contra: String) {
-
-
-    console.log(email, contra);
     this.personaservice.validarLogin(email, contra).subscribe(data => {
       if (data == true) {
         this.personaservice.num_rol(email).subscribe(data => {
-          InicioSesionComponent.rol = data;
-          this.usuariopru = data;
-          alert("Inicio Sesion con exito" + InicioSesionComponent.rol);
-        })
-        if (this.usuariopru == 2) {
-          localStorage.setItem('idProducto', this.usuariopru + "");
+          InicioSesionComponent.rol=data;
+          console.log("Inicio sesion: "+InicioSesionComponent.rol);
+          alert("Inicio Sesion con exito");
           this.router.navigate(['/app-pagina-inicio']);
-        }
-        //this.router.navigate(['/app-pagina-inicio'])
-
+        })
       } else {
         alert("No registrado")
       }
