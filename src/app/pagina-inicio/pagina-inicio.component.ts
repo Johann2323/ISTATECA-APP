3d82d3526a90624f1b8f6b233af7a00ec47c8630
Liz <lizeth.orellana.est@tecazuay.edu.ec>
2022-09-20 01:26:22 -0500

    ventana emergente

--- src/app/pagina-inicio/pagina-inicio.component.ts
+++ src/app/pagina-inicio/pagina-inicio.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { PaginaInicioService } from './pagina-inicio.service';
 import { PaginaInicio } from './pagina';
+import { Router, RouterLink } from '@angular/router';
+import { InicioSesionComponent } from '../inicio-sesion/inicio-sesion.component';
 
 @Component({
   selector: 'app-pagina-inicio',
@@ -8,18 +10,34 @@ import { PaginaInicio } from './pagina';
   styleUrls: ['./pagina-inicio.component.css']
 })
 export class PaginaInicioComponent implements OnInit {
-   
+  reporteV:string="";
   public PaginaI: PaginaInicio = new PaginaInicio();
-  paginas: PaginaInicio[]=[]
-  constructor(
-    private paginainicioService: PaginaInicioService
-    ) { }
-
+  paginas: PaginaInicio[]=[];
+  mostrar:boolean=false;
+  constructor(private paginainicioService: PaginaInicioService, private router: Router, private router1: Router) { }
+  ngDoCheck(): void {
+    this.reporteV=JSON.parse(localStorage.getItem('rol')+"");
+    console.log("Rol del Usuario: "+this.reporteV+"")
+  }
   ngOnInit(): void {
     this.paginainicioService.getLibros().subscribe(
       //response =>{this.PaginaI}
       pagina => this.paginas=pagina
     );
+  }
+  SolicitarLibro(){
+    if(parseInt(this.reporteV)==9){
+      console.log('no ha entrado');
+      alert("Inicie Sesión Primero");
+      this.router.navigate(['/']);
+    }else{
+      var overlay=document.getElementById('overlay'); 
+      overlay?.classList.add('active');
+    }
+  }
+  cerrarpopup(){
+    var overlay=document.getElementById('overlay'); 
+      overlay?.classList.remove('active');
   }
  
 }
