88d5f3e47e99b8ffa9d78a41a5e9528eba74bd47
Johann2323 <johann.arizaga.est@tecazuay.edu.ec>
2022-09-29 01:33:58 -0500

    parametrizacion

---
+++ src/app/solicitud-libro/doch.ts
@@ -0,0 +1,3 @@
+export class doch{
+    nombre_doc?:string
+}
--- src/app/solicitud-libro/solicitud-libro.component.ts
+++ src/app/solicitud-libro/solicitud-libro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { doch } from './doch';
 
 @Component({
   selector: 'app-solicitud-libro',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solicitud-libro.component.css']
 })
 export class SolicitudLibroComponent implements OnInit {
+mostrar:boolean=false;
+doch:doch[]=[]
+variable?:number
+
+documentos:doch=new doch;
+names?:string[]=[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  activarDoc(){
+    this.mostrar=true
+  }
+  desactivarDoc(){
+    this.mostrar=false
+  }
+  guardarDoc(doc:string,reg:NgForm){
+    if(doc==""){
+      alert("Ingrese un tipo de documento")
+    }else{
+      this.variable=this.doch.length+1;
+      
+
+        this.names?.push(doc)
+        console.log(this.names)
+      
+      
+
+
+    }
+    
+    
+    reg.reset();
+    
+
+  }
+
+
 
 }
